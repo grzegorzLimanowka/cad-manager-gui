815975dba8a6a5aff350a01e6d602f15554d5a20
Loteledich <loteledich@gmail.com>
2021-03-04 23:41:41 +0100

    Cannot pass values from input

---
+++ adapter.py
@@ -0,0 +1,134 @@
+import argparse
+import sys
+import datetime
+#from serial import Serial
+
+
+def communicate(dane):
+#    port = Serial(args.port, args.baud, timeout=10)
+#    port.write(str.encode(dane + '\r\n'))
+
+    encoded_data = str.encode(dane + '\r\n') 
+    print("encoded data: ", encoded_data)
+
+    print(">> tx: ", dane)
+    line = port.readline()
+    print("<< rx: ", line)
+    return line
+
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument('-port', '--port', type=str, choices=['/dev/ttyUSB0', '/dev/ttyUSB1'], default='/dev/ttyUSB0', help='Select USB port')
+parser.add_argument('-baud', '--baud', type=str, choices=['115200', '9600'], default='115200', help='Baudrate')
+
+parser.add_argument('-money-get', '--money_get', type=str, choices=["amount"], required=False, help='Get inserted cash')
+parser.add_argument('-money-return', '--money_return', type=int, default=-1, help='Return cash amount')
+parser.add_argument('-device-rst', '--device_rst', type=str, required=False, choices=['all', 'coins', 'notes'], help='reset device')
+parser.add_argument('-device-stat', '--device_stat', type=str, required=False, choices=['get'], help='return device state: ok, fault, off')
+parser.add_argument('-device-on', '--device_on', type=str, required=False, choices=['all', 'coins', 'notes'], help='...')
+parser.add_argument('-device-off', '--device_off', type=str, required=False, choices=['all', 'coins', 'notes'], help='...')
+#parser.add_argument('-coin-enable', nargs = '*', dest = 'coin_enable', help = '...')
+parser.add_argument('-coin-enable', '--coin_enable', type=int, default=-1, help='')
+parser.add_argument('-coins-get', '--coin-get', type=str, required=False, choices=['tube-type', 'tube-status', 'tube-money'], help='...')
+parser.add_argument('-adapter', '--adapter', type=str, required=False, choices=['reboot'], help='...')
+
+parser.add_argument('-device-enable', '--device_enable', type=str, required=False, choices=['all', 'coins', 'notes'], help='...')
+parser.add_argument('-device-disable', '--device_disable', type=str, required=False, choices=['all', 'coins', 'notes'], help='...')
+
+# Notes:
+parser.add_argument('-device-get', '--device_get', type=str, required=False, choices=['state'], help='...')
+parser.add_argument('-notes-get', '--notes_get', type=str, required=False, choices=['map', 'enable', 'recycler-note', 'recycler-amount'], help='...')
+#{"notes-set-enable":255}
+parser.add_argument('-notes-set-enable', '--notes_set_enable', type=str, required=False, help='...')
+
+#{"notes-set-recycler-note": 3}
+parser.add_argument('-notes-set-recycler-note', '--notes_set_recycler_note', type=str, required=False, help='...')
+
+# notes-recycler-to-cashbox
+parser.add_argument('-notes-recycler-to-cashbox', '--notes_recycler_to_cashbox', type=str, required=False, choices=['all'], help='...')
+
+# {"cashbox":"reset"}
+parser.add_argument('-cashbox', '--cashbox', type=str, required=False, choices=['get', 'reset'], help='...')
+
+
+args = parser.parse_args()
+
+
+
+print ("args: ", args)
+
+# print ("port: ", args.port)
+# print ("baudrate: ", args.baud)
+
+if (args.money_get == "amount"):
+    communicate('{"money-get":"amount"}')
+    exit
+
+if (args.money_return >= 0):
+    communicate('{"money-return":' + str(args.money_return) + '}')
+    exit
+
+if args.device_rst is not None:
+    communicate('{"device-rst":"' + str(args.device_rst) + '"}')
+    exit
+
+if args.device_stat is not None:
+    communicate('{"device-stat":"' + str(args.device_stat) + '"}')
+    exit
+
+if args.device_on is not None:
+    communicate('{"device-on":"' + str(args.device_on) + '"}')
+    exit
+
+if args.device_off is not None:
+    communicate('{"device-off":"' + str(args.device_off) + '"}')
+    exit
+
+if (args.coin_enable >= 0):
+    communicate('{"coin_enable":' + str(args.coin_enable) + '}')
+    exit
+
+if args.coin_get is not None:
+    communicate('{"coins-get":"' + str(args.coin_get) + '"}')
+    exit
+
+if args.adapter is not None:
+    communicate('{"adapter":"' + str(args.adapter) + '"}')
+    exit
+
+if args.device_enable is not None:
+    communicate('{"device-enable":"' + str(args.device_enable) + '"}')
+    exit
+
+if args.device_disable is not None:
+    communicate('{"device-disable":"' + str(args.device_disable) + '"}')
+    exit
+
+
+# Notes: 
+
+if args.device_get is not None:
+    communicate('{"device-get":"' + str(args.device_get) + '"}')
+    exit
+
+if args.notes_get is not None:
+    communicate('{"notes-get":"' + str(args.notes_get) + '"}')
+    exit
+
+if args.notes_set_enable is not None:
+    communicate('{"notes-set-enable":' + args.notes_set_enable + '}')
+    exit
+
+if args.notes_set_recycler_note is not None:
+    communicate('{"notes-set-recycler-note":' + args.notes_set_recycler_note + '}')
+    exit
+
+
+if args.notes_recycler_to_cashbox is not None:
+    communicate('{"notes-recycler-to-cashbox":"' + args.notes_recycler_to_cashbox + '"}')
+    exit
+
+if args.cashbox is not None:
+    communicate('{"cashbox":"' + args.cashbox + '"}')
+    exit
--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import os
 
 
 class Graphic:
@@ -6,10 +7,10 @@ class Graphic:
 
         # menu z lewej
         left_menu = [ [sg.Text('')],
-            [sg.B('Zarządzanie gotówką',key='o1')],
-            [sg.B('Włącz / wyłącz akceptory',key='o2')],
-            [sg.B('Włącz / wyłącz\n przyjmowanie gotówki',size=(20,2),key='o3')],
-            [sg.B('Resetuj urządzenia',key='o4')],
+            [sg.B('Zarządzanie gotówką',key='menu1')],
+            [sg.B('Włącz / wyłącz akceptory',key='menu2')],
+            [sg.B('Włącz / wyłącz\n przyjmowanie gotówki',size=(20,2),key='menu3')],
+            [sg.B('Resetuj urządzenia',key='menu4')],
                   [sg.Text('')]
         ]
         left_menu=sg.Frame('',left_menu)
@@ -18,18 +19,18 @@ class Graphic:
         #góra
         static_top = [
             [sg.Text('Port       '), sg.Input(size=(10,1)),sg.Button("PL"),sg.Button("ENG")],
-            [sg.Text('Baudrate'), sg.Listbox(values=('Opcja 1', 'Opcja 2')), sg.Button("Wykonaj")]]
+            [sg.Text('Baudrate'), sg.Listbox(values=('115200', '9600')), sg.Button("Wykonaj")]]
 
 
         #środek
 
-        button_input=  [[sg.B("Money Return",key='2')],[sg.Input(size=(10,1))]]
+        button_input=  [[sg.B("Money Return", key='2')],[sg.Input(size=(10,1),key='-Input-')]]
         inside = [
-            [sg.B('Money Get', key='1'), sg.Column(button_input, key='2')],
-            [sg.B('Coins Get', key='3'), sg.Button('Notes Get', key='4')]]
+            [sg.B('Money Get', key='1'), sg.Column(button_input)],
+            [sg.B('Coins Get', key='3'), sg.Button('Notes Get', key='4'), sg.Button('LOL')]]
         # inside2 = [
         #     [sg.B('Device Enable Coins'), sg.B('Device Enable notes')],
-        #     [sg.B('Device Disable Coins'), sg.Button('Device Disable notes')]]
+        #     [sg.B('Device Disable Coins'), sg.Button('Device Disable notes')]]        #template
         # prawo + góra
         right = [
             [sg.Frame(layout=[[sg.Column(static_top, vertical_alignment='c')]], vertical_alignment='c', title='')],
@@ -41,9 +42,13 @@ class Graphic:
 
 
         self.window = sg.Window('Cash Manager', self.layout,# default_element_size=(12, 1),
-                                size=(1000, 1000))
+                                size=(500, 500))
 
-    def work(self):
+    def left_menu(self):
+
+        flag_menu = 1 #first menu
+
+        message = ""
 
         while True:
 
@@ -52,30 +57,85 @@ class Graphic:
             if event is None or event == 'Exit':
                 break
 
-            if event == 'o1':
+
+            #left menu
+            if event == 'menu1':
+                flag_menu = 1
                 self.window['1'].update('Money Get')
                 self.window['2'].update('Money Return')
                 self.window['3'].update('Coins Get')
                 self.window['4'].update('Notes Get')
-            if event == 'o2':
+            if event == 'menu2':
+                flag_menu = 2
                 self.window['1'].update('Device On Coins')
                 self.window['2'].update('Device On Notes')
                 self.window['3'].update('Device Off Coins')
                 self.window['4'].update('Device Off Notes')
-            if event == 'o3':
+            if event == 'menu3':
+                flag_menu = 3
                 self.window['1'].update('Device Enable Coins')
                 self.window['2'].update('Device Enable notes')
                 self.window['3'].update('Device Disable Coins')
                 self.window['4'].update('Device Disable Notes')
-            if event == 'o4':
+            if event == 'menu4':
+                flag_menu = 2
                 self.window['1'].update('Adapter reboot')
                 self.window['2'].update('Device Reset All')
                 self.window['3'].update('Device Reset Coins')
                 self.window['4'].update('Device Reset Notes')
+            if event in '1234':
+                    message = self.options(event,message,flag_menu)
 
-
+            if event == 'Wykonaj':
+                self.send(message)
         self.window.close()
+
+    def options(self, event, message,flag_menu, values):
+            #menu1
+            if event == '1' and flag_menu == 1:
+                message = "python adapter.py -money-get amount" # + amount
+            if  event == '2' and flag_menu == 1:
+                message = "python adapter.py -money-return " + str(values['-Input-'])
+            if  event == '3' and flag_menu == 1:
+                message = "python adapter.py -coins-get" + "'tube-type', 'tube-status', 'tube-money'" # need fix
+            if  event == '4' and flag_menu == 1:
+                message = "python adapter.py -notes-get" + "'map', 'enable', 'recycler-note', 'recycler-amount'" #.
+            #menu2
+            if event == '1' and flag_menu == 2:
+                message = "python adapter.py -device-on coins"
+            if  event == '2' and flag_menu == 2:
+                message = "python adapter.py -device-on notes"
+            if  event == '3' and flag_menu == 2:
+                message = "python adapter.py -device-off coins"
+            if  event == '4' and flag_menu == 2:
+                message = "python adapter.py -device-off amount"
+            #menu3
+            if event == '1' and flag_menu == 3:
+                message = "python adapter.py -device-enable coins"
+            if  event == '2' and flag_menu == 3:
+                message = "python adapter.py -device-enable notes"
+            if  event == '3' and flag_menu == 3:
+                message = "python adapter.py -device-disable coins"
+            if  event == '4' and flag_menu == 3:
+                message = "python adapter.py -device-disable coins"
+            #menu4
+            if event == '1' and flag_menu == 4:
+                message = "python adapter.py -adapter reboot"
+            if  event == '2' and flag_menu == 4:
+                message = "python adapter.py -device_rst All"
+            if  event == '3' and flag_menu == 4:
+                message = "python adapter.py -device_rst Coins"
+            if  event == '4' and flag_menu == 4:
+                message = "python adapter.py -device_rst Notes"
+
+            return message
+
+    def send(self, message):
+
+        #os.system(message)
+        sg.popup(message,'MESSAGE')
+
 
 
 gui = Graphic()
-gui.work()
+gui.left_menu()
